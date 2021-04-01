2a86feae652cec862bd7f66d05612e291af64094
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-01 12:29:54 +0300

    Added class List with field first and property _Count

---
+++ Lab1_ADST/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1_ADST
+{
+    class List<T> where T: IComparable<T>
+    {
+        public Node<T> first;
+        private int _Count { get; set; } 
+    }
+}
