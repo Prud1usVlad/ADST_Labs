d76756ca36c8997d43f8e5db72b2e171edd4bf0b
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-01 12:32:36 +0300

    Renamed files and added constructor to List

--- Lab1_ADST/List.cs
+++ Lab1_ADST/List.cs
@@ -7,6 +7,12 @@ namespace Lab1_ADST
     class List<T> where T: IComparable<T>
     {
         public Node<T> first;
-        private int _Count { get; set; } 
+        private int _Count { get; set; }
+
+        public List()
+        {
+            first = null;
+            _Count = 0;
+        } // constructor 
     }
 }
