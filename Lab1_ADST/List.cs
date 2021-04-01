cc654b84c3740ce84edae0b253ac5f55a9f96d25
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-01 12:47:53 +0300

    Added AddItem method

--- Lab1_ADST/List.cs
+++ Lab1_ADST/List.cs
@@ -20,8 +20,36 @@ namespace Lab1_ADST
         public bool IsEmpty()
         {
             return (first == null) ? true : false;
-        }
+        } // IsEmpty
 
+        public void AddItem(T item) // keeps list sorted
+        {
+
+            if (IsEmpty()) // insert very first item 
+            {
+                first = new Node<T>(item);
+                _count++;
+            }
+            else
+            {
+                Node<T> current = first;
+                Node<T> previous = null;
+
+                while (current != null && item.CompareTo(current.data) > 0)
+                {
+                    previous = current;
+                    current = current.next;
+                }
+
+                if (current == first && item.CompareTo(current.data) <= 0) // insertt to the first position
+                    if (current == first && item.CompareTo(current.data) <= 0) // insrrt to the first position
+                        first = new Node<T>(item, current);
+                    else  // insert to the last and middle position
+                        previous.next = new Node<T>(item, current);
+
+                _count++;
+            }
+        } // AddItem
 
     } // List
 
