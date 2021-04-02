e598f63f8bf6401a588f81ad97b8117f419946c6
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-02 07:24:49 +0300

    Added testing code

--- Lab1_ADST/Program.cs
+++ Lab1_ADST/Program.cs
@@ -9,19 +9,31 @@ namespace Lab1_ADST
             var a = new List<int>();
 
             a.AddItem(14);
-            a.AddItem(0);
+            a.AddItem(2);
+            a.AddItem(5);
+            a.AddItem(45);
+            a.AddItem(-5);
+            a.AddItem(-4);
             a.AddItem(-45);
+            a.AddItem(4);
             a.Print();
-            Console.WriteLine(a.Search(-45));
-            Console.WriteLine(a.Search(-4));
-            a.DeleteItem(0);
-            Console.WriteLine(a.Retrieve(15));
-            Console.WriteLine(a.Retrieve(-45));
-            a.Print();
-            a.MakeEmpty();
-            a.Print();
+            Console.Write("List empty: ");
+            Console.WriteLine(a.IsEmpty());
+            Console.Write("List size: ");
             Console.WriteLine(a.ListSize());
-            
+            Console.Write("Retrieve elemet -5 (if 0 then no such element): ");
+            Console.WriteLine(a.Retrieve(-5));
+            Console.Write("Retrieve elemet 15 (if 0 then no such element): ");
+            Console.WriteLine(a.Retrieve(15));
+            Console.Write("Search elemet -5: ");
+            Console.WriteLine(a.Search(-5));
+            Console.Write("Search elemet 15: ");
+            Console.WriteLine(a.Search(15));
+
+            a.DeleteItem(-4);
+            a.DeleteItem(2);
+            a.Print();
+
         }
     }
 }
