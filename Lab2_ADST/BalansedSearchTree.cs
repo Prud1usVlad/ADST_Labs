d4312d6c2c3999d2d64cf8375940f472622c2602
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-09 17:49:19 +0300

    Added AddItem method

--- Lab2_ADST/BalansedSearchTree.cs
+++ Lab2_ADST/BalansedSearchTree.cs
@@ -6,6 +6,28 @@ namespace Lab2_ADST
 {
     class BalansedSearchTree<T> where T: IComparable
     {
+        BSTNode<T> root;
+
+        public void AddItem(T item)
+        {
+            root = AddItem_rec(item, root);
+        }
+
+        private BSTNode<T> AddItem_rec(T item, BSTNode<T> n)
+        {
+            if (n == null)
+            {
+                n = new BSTNode<T>(item);
+                n.leftSubTree = n.rightSubTree = null;
+            }
+            else if (item.CompareTo(n.data) < 0)
+                n.leftSubTree = AddItem_rec(item, n.leftSubTree);
+            else
+                n.rightSubTree = AddItem_rec(item, n.rightSubTree);
+
+
+            return n;
+        }
 
 
     }
