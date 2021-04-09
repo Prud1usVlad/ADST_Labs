7ac38cd46ab9978d63b3573ae56ce3fa722c3457
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-09 17:52:36 +0300

    Added tests for program

--- Lab2_ADST/Program.cs
+++ Lab2_ADST/Program.cs
@@ -6,7 +6,18 @@ namespace Lab2_ADST
     {
         static void Main(string[] args)
         {
-            
+            var a = new BalansedSearchTree<int>();
+
+            a.AddItem(10);
+            a.AddItem(1);
+            a.AddItem(20);
+            a.AddItem(8);
+            a.AddItem(9);
+            a.AddItem(12);
+            a.Preorder();
+            a.Inorder();
+            a.Postorder();
+
         }
     }
 }
