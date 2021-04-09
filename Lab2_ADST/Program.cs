ceb9efcbaaa42f7aa8881b530c661a4e66104dee
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-09 15:40:54 +0300

    Aded new Lab

---
+++ Lab2_ADST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_ADST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
