24c1389f0ceb6890db960b80a8ceb7614acdee27
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-26 19:51:46 +0300

    Added ArrayList class with field array and indexer

---
+++ LAB3_ADST/ArrayList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB3_ADST
+{
+    class ArrayList<T>
+    {
+        private T[] array;
+        
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+    }
+}
