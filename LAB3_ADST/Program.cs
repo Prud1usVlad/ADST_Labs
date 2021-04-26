962088dac88d69cbf659432f4a6dbd1307d9ffdc
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2021-04-26 19:29:01 +0300

    Project created

---
+++ LAB3_ADST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB3_ADST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
